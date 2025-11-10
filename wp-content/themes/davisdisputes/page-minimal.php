6e081564370ece1c0fbc111198390d9bb8dfd0c3
anna2189 <anna1618@gmail.com>
2025-11-10 16:10:29 -0500

    fix minimal page

--- wp-content/themes/davisdisputes/page-minimal.php
+++ wp-content/themes/davisdisputes/page-minimal.php
@@ -9,6 +9,17 @@ defined('ABSPATH') || exit;
 get_header();
 ?>
 
+<style>
+/* Fix for bullets appearing on minimal template */
+body ul, body li,
+.main-navigation ul, .main-navigation li,
+.footer-navigation ul, .footer-navigation li,
+.social-links li, .wp-block-social-links li {
+  list-style: none !important;
+  list-style-type: none !important;
+}
+</style>
+
 <main id="primary" class="site-main site-main--minimal">
   <?php while ( have_posts() ) : the_post(); ?>
 
